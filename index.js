2aa148a9b1866aa6593755d5a77eff7560b94c1f
sand7348 <bharnisandeep63@gmail.com>
2026-02-13 03:53:01 +0530

    Updated metadata API path for Vercel

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ app.get("/llm-output", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
-});
+// app.listen(3000, () => {
+//     console.log("Server running on http://localhost:3000");
+// });
 
 module.exports = app;
